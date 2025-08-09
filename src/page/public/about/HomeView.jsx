ca7ea15074a9fb2e4ec5eea6b6b0135637955c61
Amit-Biswas-WD <amitbiswas9282@gmail.com>
2025-08-09 14:35:27 -0700

    222

--- src/page/public/about/HomeView.jsx
+++ src/page/public/about/HomeView.jsx
@@ -1,6 +1,6 @@
 const HomeView = () => {
   return (
-    <div className="text-white text-4xl font-medium">This is Home Page 2.</div>
+    <div className="text-white text-4xl font-medium">This is Home Page 222222222.</div>
   );
 };
 
