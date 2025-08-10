2f9f38b150ac3165302da3ef6089171c961f0ebd
Amit-Biswas-WD <amitbiswas9282@gmail.com>
2025-08-09 17:56:50 -0700

    Navbar

---
+++ src/components/Button/StyleButton.jsx
@@ -0,0 +1,9 @@
+const StyleButton = ({ props }) => {
+  return (
+    <div className="text-[white] bg-[#73BFA1] rounded-4xl px-[24px] py-[12px]">
+      {props}
+    </div>
+  );
+};
+
+export default StyleButton;
--- src/loyout/template/main/NavStyle.jsx
+++ src/loyout/template/main/NavStyle.jsx
@@ -1,8 +1,26 @@
+import navImage from "../../../assets/UnoSicurezza/Home/nav_icon/Group.png";
+import StyleButton from "../../../components/Button/StyleButton";
 
 const NavStyle = () => {
   return (
-    <div className="text-3xl font-medium text-white">NavStyle</div>
-  )
-}
+    <div className="flex justify-between text-white my-8 w-[1440px] container mx-auto">
+      <div className="">
+        <img className="ml-6" src={navImage} alt="" />
+        <p className="w-6 h-8 text-[#252525]">UnoSicurezza</p>
+      </div>
+      <div className="flex gap-[70px] my-6">
+        <h2 className="font-normal text-base">Home</h2>
+        <h2 className="font-normal text-base">Servizi</h2>
+        <h2 className="font-normal text-base ">Formazione</h2>
+        <h2 className=" font-normal text-base">Chi siamo</h2>
+        <h2 className="font-normal text-base">Collabora con noi</h2>
+        <h2 className="w-[47px] h-[24px] font-normal text-base">Contattaci</h2>
+      </div>
+      <div className="">
+        <StyleButton props={"E-Learning"} />
+      </div>
+    </div>
+  );
+};
 
-export default NavStyle
+export default NavStyle;
